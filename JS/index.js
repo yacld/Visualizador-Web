4768ad4c2175bde9f0896c3c2f8a7a5e2b7fd00c
yacld <36861237+yacld@users.noreply.github.com>
2018-05-24 18:17:38 -0500

    Initial commit

---
+++ JS/index.js
@@ -0,0 +1,78 @@
+// Our Javascript will go here.
+
+document.getElementById('customFile').onchange = function(){
+
+  var file = this.files[0];
+
+  var reader = new FileReader();
+  reader.onload = function(progressEvent){
+// Entire file
+  //console.log(this.result);
+
+// By lines
+  var lines = this.result.split('\n');
+  var puntos = [];
+  var colores =  [];
+  for(var line = 0; line < lines.length; line++){
+    //~ console.log(lines[line]);
+    var linea  =  lines[line].split(" ");
+    var json = { "x": linea[0], "y": linea[1], "z": linea[2], color: linea[3] };
+    puntos.push(json);
+
+    if(!colores.includes(linea[3])){
+    colores.push(linea[3]);}
+  }
+  console.log(puntos);
+  console.log(colores);
+  three(puntos, colores);
+  };
+
+reader.readAsText(file);
+
+//~ three();
+//~ renderer.render(scene, camera);
+};
+
+var three = function(puntos, colores){
+  var scene = new THREE.Scene();
+  var camera = new THREE.PerspectiveCamera( 75, 1000/800, 0.1, 1000 );
+  var controls = new THREE.OrbitControls( camera );
+  camera.position.set(-15, -0, 500);
+
+  controls.update();
+
+  var renderer = new THREE.WebGLRenderer();
+  renderer.setSize( 1000, 800 );
+  espacio.appendChild( renderer.domElement );
+
+  //especifica las figuras y su material
+  var p = new THREE.BoxGeometry(1, 1, 1);
+  var colors = 0x0000ff;
+
+  puntos.forEach(function(punto){
+
+    var aux = parseInt(colors, 16) + parseInt(punto.color*10000);
+    var aux2 = aux.toString(16);
+    var c = new THREE.MeshBasicMaterial( {color: aux}  );
+    var puntomesh =  new THREE.Mesh( p, c);
+      puntomesh.position.x=parseInt(punto.x);
+      puntomesh.position.y=parseInt(punto.y);
+      puntomesh.position.z=parseInt(punto.z);
+      scene.add(puntomesh);
+
+    });
+
+  //funcion que coloca la escena en el navegador
+  var animate = function () {
+    requestAnimationFrame( animate );
+    //~ controls.update();
+
+    renderer.render(scene, camera);
+
+    //menu
+
+
+  };
+
+  animate();
+};
