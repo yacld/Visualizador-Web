c117004b39c0efce4009b148dadec22b8af15a0d
yacld <36861237+yacld@users.noreply.github.com>
2018-06-08 03:25:26 -0500

    estructura

--- JS/index.js
+++ JS/index.js
@@ -22,21 +22,31 @@ document.getElementById('customFile').onchange = function(){
 
 var voronoi3 = function(puntos){
   var scene = new THREE.Scene();
-  var camera = new THREE.PerspectiveCamera( 75, 1000/800, 0.1, 1000 );
+  var aspect = window.innerWidth / window.innerHeight;
+  var camera = new THREE.PerspectiveCamera( 75, aspect, 0.1, 1000 );
 
   camera.position.set(350, 350, 700);
   camera.lookAt(0, 0, 0);
-  var controls = new THREE.OrbitControls( camera );
-  // auto rotate
-  controls.autoRotate = true;
-  controls.autoRotateSpeed = 5;
-  
-
 
   var renderer = new THREE.WebGLRenderer();
   renderer.setSize( 1000, 800 );
   espacio.appendChild( renderer.domElement );
 
+  var controls = new THREE.OrbitControls( camera, renderer.domElement );
+  // auto rotate
+  controls.autoRotate = true;
+  controls.autoRotateSpeed = 15;
+
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.25;
+
+  controls.minDistance = 200;
+  controls.maxDistance = 1000;
+
+
+
+  var group = new THREE.Group();
+  scene.add(group);
 
   //especifica las figuras y su material
   var p = new THREE.BoxGeometry(1, 1, 1);
@@ -52,7 +62,7 @@ var voronoi3 = function(puntos){
     puntomesh.position.x=parseInt(punto.x);
     puntomesh.position.y=parseInt(punto.y);
     puntomesh.position.z=parseInt(punto.z);
-    scene.add(puntomesh);
+    group.add(puntomesh);
     puntosred.push(puntomesh);
   });
   //$("#progressBar").attr('aria-valuenow', 90).css({"width":"90%"}).text("90%");
